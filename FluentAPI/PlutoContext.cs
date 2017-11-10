9ec5ad2a9fbc0bc5d502f481c1580f44e70c099c
Andrew Jones <jones.gaffney1920@gmail.com>
2017-11-09 19:54:34 -0600

    First Commit

---
+++ FluentAPI/PlutoContext.cs
@@ -0,0 +1,52 @@
+namespace FluentAPI
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Id)
+                .HasColumnOrder(1);
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnOrder(2);
+
+            modelBuilder.Entity<Course>()
+                .Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(2000);
+
+            modelBuilder.Entity<Author>()
+                .HasMany(e => e.Courses)
+                .WithRequired(e => e.Author)
+                .HasForeignKey(e => e.AuthorId)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Course>()
+                .HasMany(e => e.Tags)
+                .WithMany(e => e.Courses)
+                .Map(m => m.ToTable("CourseTags").MapLeftKey("CourseId").MapRightKey("TagId"));
+
+            modelBuilder.Entity<Course>()
+                .HasRequired(e => e.Cover)
+                .WithRequiredPrincipal(e => e.Course);
+        }
+    }
+}
